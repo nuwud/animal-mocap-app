1bdbc863b4f05a710d0f4fe1a16646ea367bfc5d
Patrick Allan Wood <nuwudorder@gmail.com>
2025-03-05 17:35:17 -0800

    Fix Android build issues and component registration

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { registerRootComponent } from 'expo';
+import { AppRegistry } from 'react-native';
 import App from './App';
 
-// Register the main component
-registerRootComponent(App);
+// Register the main component with a specific name to match MainActivity.java
+AppRegistry.registerComponent('AnimalMoCapApp', () => App);
