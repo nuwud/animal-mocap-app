9871d876c54baadee3835ecde91cfa7255010505
Patrick Allan Wood <nuwudorder@gmail.com>
2025-03-06 15:49:22 -0800

    Add module-resolver to babel.config.js for better import handling

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,25 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@src': './src',
+            '@app': './src/app',
+            '@core': './src/core',
+            '@assets': './assets',
+            '@components': './src/app/components',
+            '@screens': './src/app/screens',
+            '@navigation': './src/app/navigation',
+            '@state': './src/app/state',
+            '@vision': './src/vision',
+          },
+        },
+      ],
+    ],
   };
 };
